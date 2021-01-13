eb33cd23912a4a08ef4e02bd0a292520611f00c3
Nia RAMAROSANDY <ramarosandynia@gmail.com>
2021-01-13 11:18:45 +0100

    Format articles html content

---
+++ src/htmlContentHelpers.ts
@@ -0,0 +1,30 @@
+import cheerio from 'cheerio';
+
+const imgAttributesToRemove = [
+  'data-src',
+  'data-image-name',
+  'data-image-key',
+  'data-caption',
+];
+
+export function removeEditButtons(html: string): string {
+  const $ = cheerio.load(html);
+  $('span.mw-editsection').remove();
+
+  return $('body').html() || '';
+}
+
+export function replaceImageSrc(html: string): string {
+  const $ = cheerio.load(html);
+  const images = $('img.lazyload');
+
+  images.each((i, image) => {
+    const url = $(image).attr('data-src');
+    $(image).attr('src', url || '');
+    imgAttributesToRemove.forEach((attr) => {
+      $(image).removeAttr(attr);
+    });
+  });
+
+  return $('body').html() || '';
+}
--- src/scrapers/articlesContent.ts
+++ src/scrapers/articlesContent.ts
@@ -2,6 +2,15 @@ import axios from 'axios';
 
 import { apiConfig, fsConfig } from '../config';
 import { asyncBatch, saveJSON } from '../helpers';
+import { removeEditButtons, replaceImageSrc } from '../htmlContentHelpers';
+
+type ArticleContent = {
+  title: string;
+  pageid: number;
+  text: {
+    '*': string;
+  };
+};
 
 async function getArticle(article: string) {
   try {
@@ -14,24 +23,34 @@ async function getArticle(article: string) {
     console.log('Fetching article :', article);
     const { data } = await axios.get(apiConfig.BASE_URL, { params });
 
-    return data.parse;
+    // We put inside a table just to match the required type for asyncBatch
+    return [data.parse];
   } catch (error) {
-    return {};
+    return [];
   }
+}
+
+function formatArticleContent(html: string) {
+  return replaceImageSrc(removeEditButtons(html));
 }
 
 export default async function scrapArticlesContent(
   articles: string[],
 ): Promise<void> {
-  await asyncBatch(
+  await asyncBatch<string, ArticleContent>(
     articles,
     (title) => getArticle(title),
-    (index, content) => {
+    (index, [content]) => {
+      const { text, ...rest } = content;
+      const article = {
+        ...rest,
+        html: formatArticleContent(text['*']),
+      };
       saveJSON(
         `${fsConfig.directories.ARTICLES_CONTENT}/${articles[index]
           .split('/')
           .join('++')}`,
-        content,
+        article,
       );
     },
   );
